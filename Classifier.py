abbd4c0392d27d115a7a0211e6fa8e94f1fe0929
shane1027 <sryan8@nd.edu>
2018-02-20 12:58:53 -0500

    split() function working

--- Classifier.py
+++ Classifier.py
@@ -16,7 +16,8 @@ def preprocess_heart_data(df):
     new_df = df.drop(df.index[0])
     new_df.columns = new_columns
 
-    return new_df
+    return new_df.apply(pd.to_numeric)  # convert strings to numbers
+
 
 # import the dataset as a pandas dataframe
 df = DecisionTreeTools.import_dataset()
@@ -26,4 +27,5 @@ parsed_df = preprocess_heart_data(df)
 
 features = list(parsed_df.columns.values)
 target = parsed_df.index.name
+
 
--- DecisionTreeTools.py
+++ DecisionTreeTools.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import subprocess
 
-# function for importing the dataset
+# import the dataset
 #
 # give path to data (local or url) and column to classify on)
 #
@@ -33,3 +33,37 @@ def import_dataset(path = "heart.data", index_col = 13):
             print("Saving dataset locally")
             df.to_csv(local_file)
     return df
+
+
+# split the given dataset on the feature given
+#
+# specify a threshold for continuous splits, recognized by {n} or {c} in the
+# feature name
+#
+# return a vector of the different data subsets, indexed in numerically
+# increasing order based on the class value
+# (TODO: maybe index should == classval ?)
+def split(dataset, feature, threshold):
+    # decide if continuous or nominal
+    data_vec = {}
+    if "{n}" in feature:   # nominal
+        for val in dataset[feature].unique():
+            data_vec[val] = dataset.loc[dataset[feature] == val]
+
+    else:               # continuous, use threshold split
+        data_vec["<" + str(threshold)] = dataset.loc[dataset[feature] < threshold]
+        data_vec[">=" + str(threshold)] = dataset.loc[dataset[feature] >= threshold]
+
+    return data_vec
+
+
+# calculate the information gain of a given split dataset
+#
+# data is given as a vector of pandas Series so that both continuous and
+# nominal data can be handled the same way
+#
+# also pass in the feature that was split on plz
+def inform_gain(data_vec, feature):
+
+
+    return ig_val
